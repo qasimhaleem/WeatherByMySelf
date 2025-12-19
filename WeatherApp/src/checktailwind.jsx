57668b6b2b03a1a676149d758587c64c27eebafb
Qasim Haleem <mrqasimhaleem@gmail.com>
2025-12-19 10:19:33 -0800

    start

--- WeatherApp/src/checktailwind.jsx
+++ WeatherApp/src/checktailwind.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-impor
+import { Link } from 'react-router-dom'
 
 const Checktailwind = () => {
     return (
-        <div className="bg-slate-700 flex justify-between text-center w-auto h-15">
-            <div className='text- p-4 text-white font-bold '>logo</div>
+        <div className="bg-slate-700 flex justify-between text-center items-center w-auto h-15 px-10">
+            <div className=' p-4 text-white font-bold '>logo</div>
             <div className='p-4 '>
-                <input type="text" name="item" id="item" className='active bg-whit border text-white border-gray-500 rounded-lg' />
-                <button className=' bg-blue-700  text-white mx-1 rounded-sm'>Search</button>
+                <input type="text" name="item" id="item" className=' p-2 bg-white border text-white border-gray-500 rounded-lg' />
+                <button className=' bg-gray-500  text-white mx-1 rounded-sm p-2'>Search</button>
             </div>
-            <div>navbar</div>
+            <nav className='flex gap-3 '>
+                <Link to='/about'>About</Link>
+                <Link to='/feature'>Features</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link to='/error'>Error</Link>
+            </nav>
 
         </div>
     )
--- WeatherApp/src/main.jsx
+++ WeatherApp/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Checktailwind from './checktailwind.jsx'
 import './index.css'
-
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Checktailwind />
-  </StrictMode>
+  <BrowserRouter>
+    <StrictMode>
+      <Checktailwind />
+    </StrictMode>
+  </BrowserRouter>
 )
